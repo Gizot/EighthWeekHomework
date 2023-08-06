fc7f543b3787cb47dd0ee1a67c1e3af2d8a87fb5
Gizot <tastangizem@gmail.com>
2023-08-06 19:19:42 +0300

    product class created

---
+++ ErpSystem/src/main/java/com/allianz/erpsystem/entity/CategoryEnum.java
@@ -0,0 +1,5 @@
+package com.allianz.erpsystem.entity;
+
+public enum CategoryEnum {
+    HUBUBAT, MANDIRA_URUNLERI, SARKUTERI, DOGAL_URUNLER;  ;
+}
---
+++ ErpSystem/src/main/java/com/allianz/erpsystem/entity/QuantityTypeEnum.java
@@ -0,0 +1,5 @@
+package com.allianz.erpsystem.entity;
+
+public enum QuantityTypeEnum {
+    KG,GRAM,LITRE,KAVANOZ,ADET,PAKET,KUTU,ML;
+}
